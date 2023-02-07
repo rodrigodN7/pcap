e68c1830f113efecd845edff02a68c703f9d4c09
root <root@localhost.localdomain>
2023-02-07 02:01:29 -0600

    pcap 1

--- pcap_1.py
+++ pcap_1.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+print("""
+
+What makes language?
+alphabet: set of symbols used to build words of certain languages
+lexis: aka a dictionary; a set of words the language offers its users
+syntax: a set of rules used to determine if a certain string of words form a valid sentence
+semantics: a set of rules determining if a certain phrase makes sense.
+IL: Instruction list is in fact the alphabet of a machine language.
+
+""")
+
 print('Hello from python')
 print("""
 
