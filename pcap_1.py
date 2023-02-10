7603f457604d6d017a81d2da94a0e81489b30ed5
root <rodrigod.noriega@gmail.com>
2023-02-10 12:03:37 -0600

    module 1

---
+++ pcap_1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+print("""
+
+What makes language?
+alphabet: set of symbols used to build words of certain languages
+lexis: aka a dictionary; a set of words the language offers its users
+syntax: a set of rules used to determine if a certain string of words form a valid sentence
+semantics: a set of rules determining if a certain phrase makes sense.
+IL: Instruction list is in fact the alphabet of a machine language.
+
+""")
+
+print('Hello from python')
+print("""
+
+	These are triple quotes!
+
+""")
+print('Sum is:', 7 + 10)
+<<<<<<< HEAD
+
+#end keyword
+print("My name is", "Python.", end=" ")
+print("Monty Python", end = "!!!!!!\n")
+
+#sep keyword
+print("My", "Name", "is", "Rodrigo", sep="-")
+
+#literals and variables
+print(type("2")) 
+print(type(2))
+=======
+>>>>>>> e68c1830f113efecd845edff02a68c703f9d4c09
