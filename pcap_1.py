1a6547d1c9b20f8880c8cd9c9d59789ed41fbf95
root <rodrigod.noriega@gmail.com>
2023-02-10 22:14:02 -0600

    module 1 & 2

--- pcap_1.py
+++ pcap_1.py
@@ -18,6 +18,7 @@ print("""
 
 """)
 print('Sum is:', 7 + 10)
+print('this is \\n \nthis is the effect of \\n')
 
 #end keyword
 print("My name is", "Python.", end=" ")
@@ -27,5 +28,40 @@ print("Monty Python", end = "!!!!!!\n")
 print("My", "Name", "is", "Rodrigo", sep="-")
 
 #literals and variables
-print(type("2")) 
-print(type(2))
+#function type let us know the type of data
+print(type("2")) #this is a string
+print(type(2)) #this is an integer
+print(type(7.1))
+print(0o123)
+print(type(0o123))
+print(0xff)
+print(type(0x15))
+print(0b101)
+print(type(0b101))
+print(7E8)
+print(0.000000000000000000000000007)
+print(7E-12)
+
+#NOTE: Python 3.6 has introduced underscores in numeric literals, allowing for
+#placing single underscores between digits and after base specifiers for improved readability.
+#This feature is not available in older versions of Python.
+print(111_111_111)
+print(-111_111_111)
+print(+111_111_111)
+
+#coding strings
+print('\'First\' string')
+print("'Second ' string")
+print('"Third" string')
+print("\"Fourth\" string")
+
+#boolean values
+print(True > False)
+print(True < False)
+
+#Basic operators, python as a calculator
+#+ , -, *, /, //, %, **
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
