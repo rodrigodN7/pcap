cb7b83ebbc73890ddf04c7b65ca3c634855e962d
root <rodrigod.noriega@gmail.com>
2023-02-18 13:01:37 -0600

    module 3

---
+++ pcap_3.py
@@ -0,0 +1,28 @@
+#!/ur/bin/env python3
+
+print('Equality and inequality')
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+var = 1  # Assigning 1 to var
+print(var != 0)
+print("""
+***************************************
+|Priority Table                       |
+***************************************
+|Priority	Operator	      |	
+|1		+,-		unuary|
+|2		**		      |
+|3		*x,/,//,%	      |
+|4		+,-		binary|
+|5		<,<=,>,>=	      |
+|6		==,!=		      |
+***************************************
+
+""")
