a048fa53af92bf0a98a4b59252fbf6914c3569a0
root <rodrigod.noriega@gmail.com>
2023-03-12 22:26:13 -0600

    module 3

--- pcap_3.py
+++ pcap_3.py
@@ -689,6 +689,18 @@ def find_location_list():
 		print('Elements found at index:', i)
 	else:
 		print('absent')
+
+def remove_repeated():
+
+	my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+	new_list = []
+	for number in my_list:  # Browse all numbers from the source list.
+		if number not in new_list:  # If the number doesn't appear within the new list...
+			new_list.append(number)  # ...append it here.
+	my_list = new_list[:]  # Make a copy of new_list.
+	print("The list with unique elements only:")
+	print(my_list)
+
 	
 #if_1()
 #minmax()
@@ -713,4 +725,5 @@ def find_location_list():
 #in_not_operators()
 #list_greater_value()
 #list_greater_value_2()
-find_location_list()
+#find_location_list()
+remove_repeated()
