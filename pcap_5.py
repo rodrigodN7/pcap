09b78bc53c235e11b225d4249aab96b7e9c867a1
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2024-12-18 23:24:44 +0000

    module 5

--- pcap_5.py
+++ pcap_5.py
@@ -28,6 +28,20 @@ print(pow(e, 1) == exp(log(e)))
 print(pow(2,2) == exp(2 * log(2)))
 print(log(e, e) == exp(0))
 
+
+###ceil, floor, trunc
+from math import ceil, floor, trunc
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(y))
+
 #Importing a module
 from os import *
 message = lambda:system('echo "mesasage"')
