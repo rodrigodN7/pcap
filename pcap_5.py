ebe3311ace36cd4dec0c20c8fc126e77d75ef06b
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2024-12-19 00:36:26 +0000

    module 5

--- pcap_5.py
+++ pcap_5.py
@@ -153,3 +153,51 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(choice(my_list))
 print(sample(my_list, 5))
 print(sample(my_list, 10))
+
+#The platform() function
+#The platform module lets you access the underlying platform's data, i.e., hardware, operating system, and interpreter version information.
+#This is how you can invoke it:
+#   platform(aliased = False, terse = False)
+#aliased → when set to True (or any non-zero value) it may cause the function to present the alternative underlying layer names instead of the common ones;
+#terse → when set to True (or any non-zero value) it may convince the function to present a briefer form of the result (if possible)
+from platform import platform
+print(platform())
+print(platform(1))
+print(platform(0,1))
+
+#The machine() function
+#Sometimes, you may just want to know the generic name of the processor which runs your OS together with Python and your code - a function named machine() will tell you that. As previously, the function returns a string.
+from platform import machine
+print(machine())
+
+#The processor function
+#The processor() function returns a string filled with the real processor name (if possible).
+from platform import processor
+print(processor())
+
+#The system() function
+#A function named system() returns the generic OS name as a string.
+from platform import system
+print(system())
+
+#The version function
+# The OS version is provided as a string by the version() function.
+from platform import version
+print(version())
+
+#The python_implementation and the python_version_tuple functions
+#If you need to know what version of Python is running your code, you can check it using a number of dedicated functions - here are two of them:
+
+#python_implementation() → returns a string denoting the Python implementation (expect CPython here, unless you decide to use any non-canonical Python branch)
+#python_version_tuple() → returns a three-element tuple filled with:
+#the major part of Python's version;
+#the minor part;
+#the patch level number.
+from platform import python_implementation, python_version_tuple
+
+print(python_implementation())
+
+for atr in python_version_tuple():
+    print(atr)
+#You can read all about all standard Python modules here:
+#https://docs.python.org/3/py-modindex.html
